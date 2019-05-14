f12fdca28356342b4194bdad75a6d24f507d33b1
Samba Diallo <samba.diallo@pubnub.com>
2019-05-14 11:36:37 -0700

    Updates

---
+++ precache-manifest.f53bd60a47437ed2b811f34866b3f7b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa18783bf707a5c62d1e03d5f3a14537",
+    "url": "/ReactHooksChat.PubNub/index.html"
+  },
+  {
+    "revision": "444759b672a1cc26c1b7",
+    "url": "/ReactHooksChat.PubNub/static/css/main.3d8be283.chunk.css"
+  },
+  {
+    "revision": "c0da83228ad1fb2e31be",
+    "url": "/ReactHooksChat.PubNub/static/js/2.4cefe1ea.chunk.js"
+  },
+  {
+    "revision": "444759b672a1cc26c1b7",
+    "url": "/ReactHooksChat.PubNub/static/js/main.b8513a58.chunk.js"
+  },
+  {
+    "revision": "95cd9c8d4ad5d0ad1209",
+    "url": "/ReactHooksChat.PubNub/static/js/runtime~main.29365274.js"
+  }
+]);
